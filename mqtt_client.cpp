537b8d252061274b1a2c0e2ceddb39146964360a
Unknown <danny_schneider_hessen@web.de>
2018-09-01 21:00:01 +0200

    snapshot 2nd part
    mqtt class...

--- mqtt_client.cpp
+++ mqtt_client.cpp
@@ -4,3 +4,25 @@ mqtt_client::mqtt_client()
 {
 
 }
+
+/**
+ * @brief mqtt_client::connect
+ * @param broker - IP Adress or domain-string
+ * @param port   - IP Port
+ * @return true on error
+ */
+bool mqtt_client::connect_to(std::string broker, uint16_t port)
+{
+  return tcp_client::connect_to(broker,port);
+}
+
+bool mqtt_client::publish()
+{
+
+  return false;
+}
+
+bool mqtt_client::get_state()
+{
+  return false;
+}
--- mqtt_client.h
+++ mqtt_client.h
@@ -1,12 +1,16 @@
 #ifndef MQTT_CLIENT_H
 #define MQTT_CLIENT_H
 
+#include <string>
 #include "tcp_client.h"
 
 class mqtt_client : private tcp_client
 {
 public:
   mqtt_client();
+  bool connect_to(std::string broker, uint16_t port);
+  bool publish();
+  bool get_state();
 };
 
 #endif // MQTT_CLIENT_H
