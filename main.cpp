308060e2d3f0c7256858812b9ef215e1b5e93d5d
Unknown <danny_schneider_hessen@web.de>
2018-09-01 17:45:26 +0200

    improved code quality & structure

--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
  * C++ client example using sockets
  */
 
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<string>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <cerrno>
 
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 using namespace std;
 
@@ -23,6 +24,8 @@ private:
   int port;
   struct sockaddr_in server;
 
+  constexpr static int const socket_void = -1;
+
 public:
   tcp_client() noexcept;
   bool conn(string, uint16_t);
@@ -31,7 +34,7 @@ public:
 };
 
 tcp_client::tcp_client() noexcept{
-  sock = -1;
+  sock = socket_void;
   port = 0;
   address = "";
   return;
@@ -41,21 +44,22 @@ tcp_client::tcp_client() noexcept{
  * Connect to a host on a certain port number
  */
 bool tcp_client::conn(string address , uint16_t port){
+
   //create socket if it is not already created
-  if(sock == -1){
+  if(sock == socket_void){
     //Create socket
     sock = socket(AF_INET , SOCK_STREAM , 0);
     if (sock == -1){
+      std::cout << "socket() failed: " << std::strerror(errno) << "\n";
       perror("Could not create socket");
     }
-
     cout << "Socket created " << "\n";
   }
   else{ /* OK , nothing */ }
 
   //setup address structure
   struct in_addr ipadr;
-  if(0==inet_aton(address.c_str(),&ipadr)){
+  if(0==inet_aton(address.c_str(),&ipadr)){ //domain string which needs to be resolved
     struct hostent *he;
     struct in_addr **addr_list;
 
@@ -63,21 +67,32 @@ bool tcp_client::conn(string address , uint16_t port){
     if ( (he = gethostbyname( address.c_str() ) ) == nullptr){
       //gethostbyname failed
       herror("gethostbyname");
-      cout<<"Failed to resolve hostname\n";
-
+      cout << "Failed to resolve hostname" << "\n";
       return false;
     }
 
-    //Cast the h_addr_list to in_addr , since h_addr_list also has the ip address in long format only
-    addr_list = reinterpret_cast<struct in_addr **>(he->h_addr_list);
+    {
+      cout << "Hostname: " << he->h_name << "\n";
+      if(AF_INET == he->h_addrtype)  cout << "IPv4" << " --> sizeof(" << he->h_length << ")" << "\n";
+      if(AF_INET6 == he->h_addrtype) cout << "IPv6" << " --> sizeof(" << he->h_length << ")" << "\n";
+      size_t ii = 0;
+      for(char* adr = he->h_addr_list[ii]; adr!=nullptr; adr=he->h_addr_list[ii++]){
+        /*cout << "IP: ";
+        for(size_t ij=0; ij<static_cast<size_t>(he->h_length); ++ij){
+          cout << uint16_t(uint8_t(adr[ij]));
+          if(ii<(static_cast<size_t>(he->h_length)-1))cout << ".";
+        }
+        cout << "\n";*/
+        cout << "IP: " << inet_ntoa(*reinterpret_cast<struct in_addr*>(adr)) << "\n";
+      }
+    }
 
-    for(int i = 0; addr_list[i] != nullptr; ++i){
-      //strcpy(ip , inet_ntoa(*addr_list[i]) );
-      server.sin_addr = *addr_list[i];
-
-      cout << address << " resolved to " << inet_ntoa(*addr_list[i]) << "\n";
-
-      break;
+    if(AF_INET == he->h_addrtype){
+      //Cast the h_addr_list to in_addr , since h_addr_list also has the ip address in long format only
+      addr_list = reinterpret_cast<struct in_addr **>(he->h_addr_list);
+      server.sin_addr = *addr_list[0];
+    }else{
+      cout << "IPv6 not implemented yet" << "\n";
     }
   }else{ //plain ip address
     server.sin_addr.s_addr = ipadr.s_addr;
@@ -87,7 +102,6 @@ bool tcp_client::conn(string address , uint16_t port){
   server.sin_port = htons( port );
 
   //Connect to remote server
-
   if (connect(sock , reinterpret_cast<struct sockaddr *>(&server), sizeof(server)) < 0){
     perror("connect failed. Error");
     return 1;
